1c8d32af9465f54b32ec5ef12bf4dd23e43a604b
Aditya9151 <72022250+Aditya9151@users.noreply.github.com>
2020-11-22 16:24:18 +0530

    write some stuff not working state

--- src/com/company/AI_bot.java
+++ src/com/company/AI_bot.java
@@ -1,14 +1,37 @@
 
 package com.company;
 
+class initialMethods{
+
+
+
+
+}
+
+
 public class AI_bot {
     public void botChance(String[][]board){
+        int bestScore=-9;
+        int bestMove=-1;
+        for(int i =0;i<3;i++){
+            for (int j =0;i<3;i++){
+                if(board[i][j]=="."){
+
+
+
+                }
+
+
+            }
+        }
+
 
 
 
     }
 
-    public void ai(boolean maximizing , String[][] board){
+    public int minimax(boolean maximizing , String[][] board){
+        return 0;
 
     }
 }
